40226c429a94fc45036a7a2a2ccb2dc93007c232
Roshan <roshanpanta2059@gmail.com>
2022-04-07 22:41:54 +0545

    completed constructor.php

--- constructors.php
+++ constructors.php
@@ -10,11 +10,15 @@ class human{
     public $details;
 
     function __construct($n, $a, $g, $add, $p, $h, $w){
-        $this->details = "The name of this person is: ".$n."<br>"."The age is: ".$a."The gender is: ".$g."The address is: ".$add."The Phone number is: ".$p."The height is: ".$h."And the weight is: ".$w;
-        $this->name = "The name is: ".$n;
+        $this->details = "The age is: ".$a."The gender is: ".$g."The address is: ".$add."The Phone number is: ".$p."The height is: ".$h."And the weight is: ".$w."<br>";
+        $this->name = "The name is: ".$n."<br>";
     }
 }
 $human1 = new human("Roshan", 19, "male", "manamaiju", 9869064636, "5ft", "60kg");
-echo $human1->details;
 echo $human1->name;
+echo $human1->details;
+
+$human2 = new human("Harry", 19, "male", "India", 9869321331, "4.5ft", "65kg");
+echo $human2->name;
+echo $human2->details;
 ?>
