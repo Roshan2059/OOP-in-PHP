ce96b8c21523cc052d5ed6ea3d4e493e082302c5
Roshan <roshanpanta2059@gmail.com>
2022-04-07 22:35:45 +0545

    still left to understand many things

--- constructors.php
+++ constructors.php
@@ -9,10 +9,12 @@ class human{
     public $weight;
     public $details;
 
-    function __construct($n, $a, $g, $a, $p, $h, $w){
-        $this->details = "The name of this person is: ".$n."<br>"."The age is: ".$a."The gender is: ".$g;  
+    function __construct($n, $a, $g, $add, $p, $h, $w){
+        $this->details = "The name of this person is: ".$n."<br>"."The age is: ".$a."The gender is: ".$g."The address is: ".$add."The Phone number is: ".$p."The height is: ".$h."And the weight is: ".$w;
+        $this->name = "The name is: ".$n;
     }
-    
 }
-$human1 = new human();
+$human1 = new human("Roshan", 19, "male", "manamaiju", 9869064636, "5ft", "60kg");
+echo $human1->details;
+echo $human1->name;
 ?>
