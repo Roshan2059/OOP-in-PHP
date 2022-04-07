982f8ab390bf51a3308cc69b7f7ee0e585fd0401
Roshan <roshanpanta2059@gmail.com>
2022-04-07 22:17:24 +0545

    half done of constructor.php

--- constructors.php
+++ constructors.php
@@ -1,3 +1,18 @@
 <?php
+class human{
+    public $name;
+    public $age;
+    public $gender;
+    public $address;
+    public $phone;
+    public $height;
+    public $weight;
+    public $details;
 
+    function __construct($n, $a, $g, $a, $p, $h, $w){
+        $this->details = "The name of this person is: ".$n."<br>"."The age is: ".$a."The gender is: ".$g;  
+    }
+    
+}
+$human1 = new human();
 ?>
